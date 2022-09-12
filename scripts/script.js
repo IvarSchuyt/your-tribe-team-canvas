861132be26d7b3e6938f33a1ea7a5a8f0f4259ea
Romer <romer@nostal.dev>
2022-09-12 16:55:02 +0200

    Basis opzet

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const cardElement = document.querySelector(".js_card");
+const nameElement = document.querySelector(".js_name");
+const imagelement = document.querySelector(".js_image");
+
+document.querySelectorAll(".js_bin li").forEach((element) => {
+  element.addEventListener("click", (event) => {
+    const name = event.target.innerText;
+    nameElement.innerText = name;
+    imagelement.src = `/assets/${name.toLowerCase()}.jpg`;
+    imagelement.alt = `Portret van ${name}`;
+  });
+});
