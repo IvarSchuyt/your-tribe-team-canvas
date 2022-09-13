3dfa6358bde24d9db6b1c4dd881954875f631353
Romer <romer@nostal.dev>
2022-09-13 12:17:08 +0200

    Clean up content

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,11 @@
-// Retreiving all card elemenets.
 const cardElement = document.querySelector(".js_card");
 const nameElement = document.querySelector(".js_name");
 const imagelement = document.querySelector(".js_image");
 
-// Looping through all li's and adding a click event listener.
 document.querySelectorAll(".js_bin li").forEach((element) => {
   element.addEventListener("click", (event) => {
     const name = event.target.innerText;
 
-    // If card is already visible, do this and then return.
     if (cardElement.classList.contains("-animate")) {
       cardElement.classList.remove("-animate");
 
@@ -32,10 +29,8 @@ document.querySelectorAll(".js_bin li").forEach((element) => {
 });
 
 function updateCard(name) {
-  // Update card text with new name.
   nameElement.innerText = name;
 
-  // Update card image source and alt text.
   imagelement.src = `./assets/${name.toLowerCase()}.webp`;
   imagelement.alt = `Portret van ${name}`;
 }
