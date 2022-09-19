6cfa9996342ea358545ccff33548756a2c2cf0d9
Romer <romer@nostal.dev>
2022-09-19 12:47:19 +0200

    Href instead of data attribute

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ document.querySelectorAll(".js_bin li").forEach((element) => {
   element.addEventListener("click", (event) => {
     event.preventDefault();
     const name = event.target.innerText;
-    const link = event.target.dataset.url;
+    const link = event.target.closest("a").href;
 
     if (cardElement.classList.contains("-animate")) {
       cardElement.classList.remove("-animate");
